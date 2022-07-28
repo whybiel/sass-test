69c138948801d061ab52836d8a768ea4b6824def
gabrielvnw <gabrimoraes2004@gmail.com>
2022-07-28 00:40:30 -0300

    começando estudo de SASS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style/ball.css"
 
-function App() {
-  return (
+export default function App(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ball"></div>
+      <div className="ball2"></div>
+      <div className="ball3"></div>
     </div>
-  );
+  )
 }
-
-export default App;
